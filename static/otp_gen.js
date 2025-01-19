712ddb0fd06d8dc812fd8a481b9db748282b31fb
felixgao-0 <76596162+felixgao-0@users.noreply.github.com>
2025-01-18 19:47:57 -0500

    revamp generation, use authenticator instead of totp, move to its own function

--- static/otp_gen.js
+++ static/otp_gen.js
@@ -1,21 +1,48 @@
-let totp = null;
-const secret = 'JA7PUOR62RDWHS7S';
+//const totp = window.otplib.totp;
+const authenticator = window.otplib.authenticator;
+const globalSecret = 'UZN2D6GDLLPQKP73';
 
-function getTOTPCode(secret) {
-    const currentTime = Math.floor(Date.now() / 1000);
-
-    return {
-        code: totp.generate(secret),
-        step: totp.options.step
-    }
+function getFutureOtpCode(secret) {
+    authenticator.options = { epoch: Date.now() + authenticator.options.step * 1000 };
+    return authenticator.generate(secret);
 };
+
+function displayCodes() {
+    authenticator.options = { epoch: Date.now() };
+    const otpCode = authenticator.generate(globalSecret);
+    const nextOtpCode = getFutureOtpCode(globalSecret);
+
+    const otpCodeField = document.getElementById('code');
+    const nextOTPCodeField = document.getElementById('next-code');
+
+    if (otpCodeField.innerText !== otpCode) {
+        // Split it in half so it looks cool :)
+        const optSplit = Math.ceil(otpCode.length / 2);
+        const nextOtpSplit = Math.ceil(nextOtpCode.length / 2);
+
+        otpCodeField.innerText = otpCode.slice(0, optSplit) + ' ' + otpCode.slice(optSplit);
+        nextOTPCodeField.innerText = nextOtpCode.slice(0, nextOtpSplit) + ' ' + otpCode.slice(nextOtpSplit);
+    }
+
+    const timeLeft = authenticator.timeRemaining();
+
+    console.log(otpCode, "-", nextOtpCode, "-", timeLeft, "secs until regen");
+    document.getElementById('progress-bar').style.width = timeLeft / 30 * 100 + '%';
+
+
+    if (timeLeft <= 5) {
+        otpCodeField.classList.add("flash");
+    } else if (otpCodeField.classList.contains("flash")) {
+        otpCodeField.classList.remove("flash");
+    }
+}
+
 
 document.addEventListener('DOMContentLoaded', (event) => {
     console.log('DOM fully loaded :D');
-    totp = window.otplib.totp;
-    totp.options = { step: 30 };
+    authenticator.options = { step: 30 };
 
-    document.querySelectorAll('.copy-button').forEach((copyBtn) => {
+    document.querySelectorAll('.copy-btn').forEach((copyBtn) => {
         copyBtn.addEventListener('click', () => {
             const otpCode = copyBtn.textContent;
 
@@ -27,22 +54,8 @@ document.addEventListener('DOMContentLoaded', (event) => {
             });
         });
     });
-        
-    setInterval(() => {
-        otpInfo = getTOTPCode(secret);
-        otpCodeField = document.getElementById('code');
-
-        otpCodeField.innerText = otpInfo.code
-
-        const currentTime = Math.floor(Date.now() / 1000);
-        const timeLeft = totp.options.step - (currentTime % totp.options.step);
-        document.getElementById('progress-bar').style.width = timeLeft / 30 * 100 + '%';
-
-
-        if (timeLeft <= 5) {
-            otpCodeField.classList.add("flash");
-        } else if (otpCodeField.classList.contains("flash")) {
-            otpCodeField.classList.remove("flash");
-        }
-    }, 1000);
+    
+    // Run immediately so no need to wait for za first interval
+    displayCodes();
+    setInterval(displayCodes, 1000);
 });
